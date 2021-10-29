a7b6ef33a35d7814f3caf4efcfc5e2a02d6acfeb
TANU WIJAYA <tanuxi@gmail.com>
2021-10-29 16:41:03 +0700

    game-ku projek UTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
+    TournamentComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
